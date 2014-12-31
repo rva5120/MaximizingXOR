34f1390ff505f5867e05e21ab420aa4aece6d683
Raquel Alvarez <rva5120@gmail.com>
2014-12-31 04:22:03 -0600

    Max XOR final

--- MaximizingXOR/main.cpp
+++ MaximizingXOR/main.cpp
@@ -5,11 +5,53 @@
 //  Created by Raquel Alvarez on 12/31/14.
 //  Copyright (c) 2014 R. All rights reserved.
 //
-
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+ * Complete the function below.
+ */
+int maxXor(int l, int r)
+{
+    int maxV = -1000;
+    int iXorj = 0;
+    
+    for (int i = l; i <= r; i++)
+    {
+        for (int j = i; j <= r; j++)
+        {
+            iXorj = i^j;
+            maxV = (iXorj > maxV) ? iXorj:maxV;
+        }
+    }
+    return maxV;
+}
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int res;
+    int _l;
+    cin >> _l;
+    
+    int _r;
+    cin >> _r;
+    
+    res = maxXor(_l, _r);
+    cout << res;
+    
     return 0;
 }
