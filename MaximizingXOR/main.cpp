2fe9050c00e35a46b3962917b20ba8559ae37194
Raquel Alvarez <rva5120@gmail.com>
2014-12-31 04:21:07 -0600

    Initial Commit

---
+++ MaximizingXOR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MaximizingXOR
+//
+//  Created by Raquel Alvarez on 12/31/14.
+//  Copyright (c) 2014 R. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
